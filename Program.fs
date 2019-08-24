4c5515b4b6b565627fc20d7f34caa98b8b83301c
UnoSD <unosd.dev@gmail.com>
2019-08-24 11:57:44 +0100

    Code clean up and refactoring

--- Program.fs
+++ Program.fs
@@ -5,24 +5,41 @@ open MetadataExtractor
 open MetadataExtractor.Formats.Exif
 open System.IO
 
+type Config =
+    {
+        culture : string
+        format : string
+        threshold : TimeSpan
+        dryRun : bool
+    }
+
+let config =
+    {//{|
+        culture = "it-IT"
+        format = "dd-MMM-yyyy"
+        threshold = TimeSpan.FromHours(1.)
+        dryRun = false
+    }//|}
+
+let multipleDaysFormat (date : DateTime) format = sprintf "%i-%s" date.Day format
+
+type Metadata = MetadataExtractor.Directory
+
 [<EntryPoint>]
 let main argv =
     
     let (|File|Folder|) path =
         match path with
-        | x when File.Exists(x) -> File x
+        | x when File.Exists(x)      -> File x
         | x when Directory.Exists(x) -> Folder x
-        | x -> failwithf "Invalid file or direcyory: %s" x 
-    
-    let getFilesFrom folder =
-        Directory.EnumerateFiles(folder) |> Seq.toList
+        | x                          -> failwithf "Invalid file or direcyory: %s" x 
     
     let getFiles path =
         match path with
-        | File file -> [ file ]
-        | Folder folder -> getFilesFrom folder
+        | File file     -> [ file ]
+        | Folder folder -> Directory.EnumerateFiles(folder) |> Seq.toList
     
-    let getTimestamp (data : IReadOnlyList<MetadataExtractor.Directory>) =
+    let getTimestamp (data : IReadOnlyList<Metadata>) =
         data |>
         Seq.choose (fun d -> match d with
                              | :? ExifSubIfdDirectory as x ->
@@ -33,33 +50,14 @@ let main argv =
         Seq.exactlyOne
     
     let formatDate (dateTime : DateTime) =
-        dateTime.ToString("dd-MMM-yyyy", new CultureInfo("it-IT"))
+        dateTime.ToString(config.format, new CultureInfo(config.culture))
     
-    let getNewFileName fileName (extension : string) formattedDate =
-        sprintf "%s.%s%s" formattedDate fileName (extension.ToLower()) //fileName 
-    
-    let files =
-        argv |>
-        Array.fold (fun x y -> getFiles y @ x) [] |>
-        List.map (fun fileName -> (new FileInfo(fileName), ImageMetadataReader.ReadMetadata(fileName))) |>
-        List.map (fun (file, data) -> (file, getTimestamp data)) |>
-        List.map (fun (file, dateTime) -> (file,
-                                           dateTime,
-                                           getNewFileName (Path.GetFileNameWithoutExtension(file.Name))
-                                                          file.Extension
-                                                          (dateTime |> formatDate))) |>
-        List.sortBy (fun (_, dateTime, _) -> dateTime)
-    
-    let updateKvp (map : Map<DateTime, ('a * DateTime * 'c) list>) fileInfo dateTime newName =
-        // More C#-ish, but cleaner
-        
-        // maxBy instead of sort and then pick...
-        
-        let (referenceDateTime, list) = (map |> Seq.sortByDescending (fun kvp -> kvp.Key) |> Seq.head).Deconstruct()
-        // More idiomatic but more boilerplate
-        //let (y, z) = match (map |> Seq.sortByDescending (fun kvp -> kvp.Key) |> Seq.head) with | KeyValue (a, b) -> (a, b) 
-        // Idiomatic and less boilerplate
-        let (KeyValue(referenceDateTime, list)) = map |> Seq.sortByDescending (fun kvp -> kvp.Key) |> Seq.head
+    let getNewFileName (fileInfo : FileInfo) formattedDate =
+        fileInfo.Extension.ToLower() |>
+        sprintf "%s.%s%s" formattedDate (fileInfo.Name |> Path.GetFileNameWithoutExtension)
+
+    let updateFolderGroup (map : Map<DateTime, ('a * DateTime * 'b) list>) fileInfo dateTime newName =
+        let (KeyValue(referenceDateTime, list)) = map |> Seq.maxBy (fun kvp -> kvp.Key)
         
         map |>
         Map.remove referenceDateTime |>
@@ -72,54 +70,64 @@ let main argv =
     
     let lastDateAddedIsAwayFrom dateTime (map : Map<DateTime, 'b>) =
         map |>
-        Seq.sortByDescending (fun kvp -> kvp.Key) |>
-        Seq.head |>
-        (function KeyValue (key, _) -> dateTime - key > TimeSpan.FromHours(1.))
+        Seq.maxBy (fun kvp -> kvp.Key) |>
+        (function KeyValue (key, _) -> dateTime - key > config.threshold)
     
-    let updateMap map fileInfo dateTime newName =
+    let addToGroups map fileInfo dateTime newName =
         match map with
         | Empty                                              -> Map [(dateTime, [(fileInfo, dateTime, newName)])]
         | NonEmpty map when map |>
                             lastDateAddedIsAwayFrom dateTime -> map.Add (dateTime, [(fileInfo, dateTime, newName)])
-        | NonEmpty map                                       -> updateKvp map fileInfo dateTime newName
+        | NonEmpty map                                       -> updateFolderGroup map fileInfo dateTime newName
+    
+    let createDirectory =
+        match config.dryRun with
+        | true  -> printfn "Creating directory %s"
+        | false -> Directory.CreateDirectory >> ignore
+        
+    let moveTo path (fileInfo : FileInfo) =
+        match config.dryRun with
+        | true  -> printfn "%s -> %s" fileInfo.Name path
+        | false -> path |> fileInfo.MoveTo
     
     let processImageGroup (list : (FileInfo * DateTime * string) list) =
-        let maxDate = list |> List.maxBy (fun (_, dateTime, _) -> dateTime) |> (fun (_, x, _) -> x)
-        let minDate = list |> List.minBy (fun (_, dateTime, _) -> dateTime) |> (fun (_, x, _) -> x)
+        let getDateBy func =
+            list |> func (fun (_, dateTime, _) -> dateTime) |> (fun (_, x, _) -> x)
+        
+        let maxDate = getDateBy List.maxBy
+        let minDate = getDateBy List.minBy
         
         let daysString =
             match minDate.Day = maxDate.Day with
-            | true -> minDate.ToString("dd-MMM-yyyy", new CultureInfo("it-IT")) |> string
-            | false -> maxDate.ToString("dd-MMM-yyyy", new CultureInfo("it-IT")) |> sprintf "%i-%s" minDate.Day 
+            | true  -> minDate |> formatDate
+            | false -> maxDate |> formatDate |> multipleDaysFormat minDate
             
         let (fileInfo, _, _) = list.Head
         
         let getNewPath newName =
             let basePath =
-                match list with
-                //| [] | [_] -> 
-                | _ :: _ :: _ -> let folderPath = Path.Combine(fileInfo.Directory.FullName, daysString)
-                                 if Directory.Exists(folderPath) then
-                                    ()
-                                 else
-                                    Directory.CreateDirectory(folderPath) |> ignore
-                                 folderPath
-                | _ -> fileInfo.Directory.FullName 
+                match list.Length with
+                | 0 | 1 -> fileInfo.Directory.FullName
+                | _ -> let folderPath = Path.Combine(fileInfo.Directory.FullName, daysString)
+                       if Directory.Exists(folderPath) then
+                          ()
+                       else
+                          createDirectory folderPath
+                       folderPath
             
             Path.Combine(basePath, newName)
         
         list |>
-        List.map (fun (fileInfo, _, newName) -> (fileInfo, getNewPath newName)) |>
-        //List.iter (fun (fileInfo, newPath) -> printfn "%A -> %A" fileInfo.Name newPath)
-        List.iter (fun (fileInfo, newPath) -> fileInfo.MoveTo(newPath))
+        List.map  (fun (fileInfo, _, newName) -> (fileInfo, getNewPath newName)) |>
+        List.iter (fun (fileInfo, newPath) -> fileInfo |> moveTo newPath)
     
-    files |>
-    List.fold (fun map (fileInfo, dateTime, newName) -> updateMap map fileInfo dateTime newName) Map.empty |>
-    Seq.map (function KeyValue (_, v) -> v) |>
-    Seq.iter processImageGroup
-    
-    
-    //file.MoveTo(newFilename)
-    //File.Move(file.FullName, Path.Combine(file.DirectoryName, newFilename))
-    
+    argv |>
+    Array.fold  (fun fileList path -> getFiles path @ fileList) [] |>
+    List.map    (fun fileName -> (FileInfo(fileName), ImageMetadataReader.ReadMetadata(fileName) |> getTimestamp)) |>
+    List.map    (fun (file, dateTime) -> (file, dateTime, dateTime |> formatDate |> getNewFileName file)) |>
+    List.sortBy (fun (_, dateTime, _) -> dateTime) |>
+    List.fold   (fun map (fileInfo, dateTime, newName) -> addToGroups map fileInfo dateTime newName) Map.empty |>
+    Seq.map     (function KeyValue (_, v) -> v) |>
+    Seq.iter    processImageGroup
+        
     0
